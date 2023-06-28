1a410d3255309777ed8af2a518404a00d5d2345c
ZhenShen-1 <e_afanasev@mail.ru>
2023-06-28 20:53:18 +0500

    add branch lambda

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,23 @@ public class Main {
     public static void main(String[] args) {
         List<Person> persons = new ArrayList<>();
 
-        persons.add(new Person("Иван", "Иванов", 47));
-        persons.add(new Person("Евгений", "Евгенов", 28));
-        persons.add(new Person("Игорь", "Игорев", 32));
-        persons.add(new Person("Анна", "Анннова", 60));
+        persons.add(new Person("Иван", "Иванов-Соков", 47));
+        persons.add(new Person("Евгений", "Евгенов-Аннова", 28));
+        persons.add(new Person("Игорь", "Игорев-Аннова-Соков", 32));
+        persons.add(new Person("Анна", "Аннова", 60));
         persons.add(new Person("Борис", "Соков", 13));
 
-        Collections.sort(persons, new PersonComparator());
+        Collections.sort(persons, (o1, o2) -> {
+            int length1 = o1.getSurname().split("-").length;
+            int length2 = o2.getSurname().split("-").length;
+            if(length1 < length2) {
+                return -1;
+            }
+            if(length1 > length2) {
+                return 1;
+            }
+            return o1.getAge() - o2.getAge();
+        });
 
         for (Person person : persons) {
             System.out.println(person);
